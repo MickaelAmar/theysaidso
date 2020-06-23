8ae058ef22cbcba851a9604b5738fb79ac5ff7f9
Mickael <mickael.amar@epitech.eu>
2020-06-23 15:28:32 +0200

    File organization

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Quote;
+use App\Helpers\FileHelper;
 use App\Helpers\StringHelper;
+use PhpParser\Node\Scalar\MagicConst\File;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,7 +19,6 @@ TODO :
 - Is there another way to handle params ? accessors ?
 - Manage cache
 - Add tests
-- Put statements in organized and logical files (everything's done in the this controller action now)
 - Deploy
 - BONUS : Set up a live documentation/test page (Swagger ?)
 - BONUS : Make a seeder script to populate a db table with quote references and manipulate it from an entity/model
@@ -24,6 +26,12 @@ TODO :
 
 class QuoteController extends AbstractController
 {
+    private $quote_entity;
+
+    public function __construct(Quote $quote_entity)
+    {
+        $this->quote_entity = $quote_entity;
+    }
 
     /**
      * @Route("/quote/{author_slug}", methods="GET")
@@ -32,25 +40,14 @@ class QuoteController extends AbstractController
      * @param KernelInterface $kernel
      * @return JsonResponse
      */
-    public function show(string $author_slug, Request $request, KernelInterface $kernel)
+    public function show(string $author_slug, Request $request)
     {
-        // Json response
-        $shout_quotes = [];
-
         // Get parameters
-        $limit = $request->query->get('limit', 1);
+        $limit = $request->query->get('limit', 10);
         $author = ucwords(str_replace('-', ' ', $author_slug));
 
-        // Get quotes references from json file
-        $resource_dir_path = $kernel->getProjectDir() . '/resources/';
-        $quotes = json_decode(file_get_contents($resource_dir_path . 'quotes.json'), true)['quotes'];
-
-        // Preparing response
-        foreach ($quotes AS $quote) {
-            if ($quote['author'] === $author && count($shout_quotes) < $limit) {
-                $shout_quotes[] = StringHelper::shout($quote['quote']);
-            }
-        }
+        // Json response
+        $shout_quotes = $this->quote_entity->findByAuthor($author, $limit);
 
         return $this->json($shout_quotes);
     }
